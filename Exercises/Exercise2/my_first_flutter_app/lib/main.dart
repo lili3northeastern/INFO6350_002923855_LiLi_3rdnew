f2d6af51f9dd96597f1102d79b0b24dd86f1b18b
lili3northeastern <98289644+lili3northeastern@users.noreply.github.com>
2023-12-03 14:47:37 -0800

    update Exercise2

--- Exercises/Exercise2/my_first_flutter_app/lib/main.dart
+++ Exercises/Exercise2/my_first_flutter_app/lib/main.dart
@@ -12,8 +12,49 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My First Flutter App'),
         ),
-        body: Center(
-          child: Text('Hello, Flutter!'),
+        body: Row(
+          children: [
+            // NavigationRail
+            NavigationRail(
+              selectedIndex: 0,
+              onDestinationSelected: (int index) {
+                // Handle navigation item selection
+                print('Item $index selected');
+              },
+              labelType: NavigationRailLabelType.selected,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  selectedIcon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  selectedIcon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+            ),
+            // Main content area
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Text('Hello, Flutter!'),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle button click
+                      print('Button pressed!');
+                    },
+                    child: Text('Click Me'),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
